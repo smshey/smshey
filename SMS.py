0c0eeb5b38fd314851ee5f7e208aa7a52e7f24c4
smshey <106284702+smshey@users.noreply.github.com>
2022-05-26 03:57:56 +0430

    Add files via upload

---
+++ SMS.py
@@ -0,0 +1,9 @@
+from nexmo import Sms
+
+sms = Sms(key=ed320a43, secret=OlUMCY1dswQYQ9FD)
+
+sms.send_message({
+            "from": ADLIRAN,
+            "to": 989926834697,
+            "text": "TEST",
+})
